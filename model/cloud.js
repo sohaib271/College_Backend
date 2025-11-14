cb271633c6212816df6212b9e9caf8a8084af316
sohaib271 <sohaibsheikh6299@gmail.com>
2025-11-14 21:54:09 +0500

    error fixed

--- model/cloud.js
+++ model/cloud.js
@@ -4,8 +4,8 @@ import multer from "multer"
 
 cloudinary.config({
   cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-  api_secret:process.env.CLOUDINARY_API_SECRET,
-  api_key:process.env.CLOUDINARY_API_KEY
+  api_key:process.env.CLOUDINARY_API_KEY,
+  api_secret:process.env.CLOUDINARY_API_SECRET
 });
 
 const storage=new CloudinaryStorage({
