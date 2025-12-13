cb596fc4675624d9c3fbc90ac070663802508838
sohaib271 <sohaibsheikh6299@gmail.com>
2025-12-13 20:53:00 +0500

    upd

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ app.use(cors({
   credentials:true
 }));
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
 app.get("/college",(req,res)=>{
   res.send("This is my server");
